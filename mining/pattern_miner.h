8487c9d2a9b707edde775aa14dce86e049ab4310
Raphael Gomes Santos <raphaelgoms@gmail.com>
2023-08-24 18:47:45 -0300

    refatoring pattern_miner

---
+++ mining/pattern_miner.h
@@ -0,0 +1,40 @@
+#include "alglib/stdafx.h"
+#include "alglib/dataanalysis.h"
+
+#include <vector>
+#include <map>
+#include <math.h>
+#include <random>
+
+#define PI 3.1415926535897932384626433832795029
+
+using namespace std;
+using namespace alglib;
+
+extern int g_number_of_used_vars;
+
+class PatternMiner {
+
+	vector<vector<double>> dataSet;
+	vector<vector<double>> getClusterPoints(int k, integer_1d_array cidx);
+
+	kmeansreport kMeans_alglib(const vector<vector<double>> &data, int k, vector<double> min, vector<double> max);
+	kmeansreport xMeans_alglib(const vector<vector<double>> &data, int kMax, const vector<double> &min, const vector<double> &max);
+
+	int improveStructure(int k, const kmeansreport &clusters, int M, const vector<double> &min, const vector<double> &max);
+	double logLikelihood(int R, int Rn, double variance, double M, double K);
+ 	
+	double average(const vector<double> &data);
+	double variance(const vector<vector<double>> &data, const vector<double> &centroid);
+    double distance(const vector<double> & v1, const vector<double> & v2);
+	double distance(double * v1, double *  v2, size_t vsize);
+
+	inline double normalize(const double &value, const double &lower_bound, const double &upper_bound) {
+    	return (value - lower_bound) / (upper_bound - lower_bound);
+	}
+	
+public:
+	// return the list de clusters as partterns
+	vector<map<int, double> > extractPatterns(vector<vector<double>> elite, const vector<double>& lower_bound, const vector<double>& upper_bound, int k=0);
+};
+
